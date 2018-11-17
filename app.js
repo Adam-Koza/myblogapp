65ac7948bc185618866a751ae006e9c418117bd7
Adam-Koza <adam.koza@mail.utoronto.ca>
2018-11-17 10:28:52 -0500

    Authentication Lab.

--- app.js
+++ app.js
@@ -12,9 +12,22 @@ var editRouter = require('./routes/posts');
 var deleteRouter = require('./routes/posts');
 var database = require('./database/database');
 
+var passport                = require("passport"),
+    bodyParser              = require("body-parser"),
+    User                    = require("./models/user"),
+    LocalStrategy           = require("passport-local"),
+    passportLocalMongoose   = require("passport-local-mongoose")
+
 database();
 
 var app = express();
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(require("express-session")({
+    secret:"Rusty is the best og in the world",
+    resave: false,
+    saveUninitialized: false
+}));
 
 // Set layout
 app.set('layout', 'layout')
@@ -23,13 +36,79 @@ app.set('layout', 'layout')
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+app.use(passport.initialize());
+app.use(passport.session());
+// 
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+
+app.get("/",function(req,res){
+    res.render("home");
+});
+
+app.get("/secret",isLoggedIn, function(req, res){
+  res.render('index', {
+    title: 'Blog Post App',
+    app: {
+      message: 'Welcome to my blog post app',
+    }
+  });
+});
+
+// Auth Routes
+
+app.get("/register", function(req, res){
+    res.render("register");
+});
+//handling user sign up
+app.post("/register", function(req, res){
+User.register(new User({username:req.body.username}),req.body.password, function(err, user){
+       if(err){
+            console.log(err);
+            return res.render('register');
+        } //user stragety
+        passport.authenticate("local")(req, res, function(){
+            res.redirect("/secret"); //once the user sign up
+       }); 
+    });
+});
+
+// Login Routes
+
+app.get("/login", function(req, res){
+    res.render("login");
+})
+
+// middleware
+app.post("/login", passport.authenticate("local",{
+    successRedirect:"/secret",
+    failureRedirect:"/login"
+}),function(req, res){
+    res.send("User is "+ req.user.id);
+});
+
+app.get("/logout", function(req, res){
+    req.logout();
+    res.redirect("/");
+});
+
+
+function isLoggedIn(req, res, next){
+    if(req.isAuthenticated()){
+        return next();
+    }
+    res.redirect("/login");
+}
+
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(expressLayouts);
-app.use('/', indexRouter);
 app.use('/blog', postsRouter);
 app.use('/blog/edit', editRouter);
 app.use('/blog/delete', deleteRouter);
